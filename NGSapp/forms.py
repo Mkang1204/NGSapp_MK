3ddcae2d6225a875deb8170ee4e9b4701bb7c0a8
Mkang1204 <mengjiakang2017@u.northwestern.edu>
2018-11-26 17:04:23 -0600

    Before making changing to accounts app + Added form.py to NGSapp and accounts"

---
+++ NGSapp/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from NGSapp.models import Run
+
+
+class HomeForm(forms.ModelForm):
+    post = forms.CharField(widget=forms.TextInput(
+        attrs={
+            # 'class': 'form-control',
+            # 'placeholder': 'Write a post...'
+        }
+    ))
--- NGSapp/views.py
+++ NGSapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Run
 from django.http import HttpResponse
 
+from NGSapp.forms import HomeForm
+ 
 # Create your views here.
 def home(request):
     return render(request, 'home.html',{})
